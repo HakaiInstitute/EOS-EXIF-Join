49695cd2a94e84cc062be98c2a061c0d60a7b591
Taylor Denouden <taylordenouden@gmail.com>
2020-09-09 13:12:24 -0400

    Change project link

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     version=os.environ['VERSION'],
     packages=['join_eos_exif'],
     install_requires=['numpy', 'pandas'],
-    url='https://github.com/HakaiInstitute/joinEOStoEXIF',
+    url='https://github.com/HakaiInstitute/join-eos-exif',
     license='MIT',
     author='Taylor Denouden',
     author_email='taylor.denouden@hakai.org',
